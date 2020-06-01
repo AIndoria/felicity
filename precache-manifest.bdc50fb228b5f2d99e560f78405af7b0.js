8da8fee80da8421651ce2dcd0aec821ac9516a97
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-06-01 01:57:45 -0700

    Updates

---
+++ precache-manifest.bdc50fb228b5f2d99e560f78405af7b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ff94998b07c5621e3bccf013d2846a",
+    "url": "/felicity/index.html"
+  },
+  {
+    "revision": "a8b737dc3944b45e2895",
+    "url": "/felicity/static/css/main.729c4d9a.chunk.css"
+  },
+  {
+    "revision": "9a3d2ea001d49d5df88c",
+    "url": "/felicity/static/js/2.feba0ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/felicity/static/js/2.feba0ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b737dc3944b45e2895",
+    "url": "/felicity/static/js/main.fbb48be4.chunk.js"
+  },
+  {
+    "revision": "5d5e5296297a4e182dfb",
+    "url": "/felicity/static/js/runtime-main.83b8b67e.js"
+  },
+  {
+    "revision": "82840f2fc024b919c4af14c9af16e757",
+    "url": "/felicity/static/media/dns_day.82840f2f.jpg"
+  },
+  {
+    "revision": "cb35d675a17afe3697a8a599de100ec9",
+    "url": "/felicity/static/media/dns_night.cb35d675.jpg"
+  }
+]);
